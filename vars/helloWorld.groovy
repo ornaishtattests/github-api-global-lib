e8bd409a492180db799dc50686e2d0ad6fdddf26
ornaishtattests <95538719+ornaishtattests@users.noreply.github.com>
2022-12-31 20:27:20 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,8 +1,5 @@
 def call(Map config = [:]) {
     rtServer (
-        id: "ARTIFACTORY_SERVER",
-        url: 'https://ornaishtat.jfrog.io/artifactory/',
-        username: 'admin',
-        password: "${TOKEN}",    
+        id: "saas", 
     )
 }
